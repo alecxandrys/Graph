65871fc3f41aa156cd9bf36071e2893ef611230b
alecxandrys <alecxandrys@gmail.com>
2016-10-25 20:02:12 +0300

    Lab1:Work finished

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,13 @@ import javax.swing.*;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.TableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Main implements Runnable {
 
     private int del;
-    int G1[][];
+    private int G1[][];
 
     public static void main(String args[]) {
         SwingUtilities.invokeLater(new Main());
@@ -32,87 +30,54 @@ public class Main implements Runnable {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 G1[i][j] = (int) (Math.random() * 2);
+                //G1[i][j]=10*i+j;
             }
         }
 
-        JTable table = new JTable(new TableModel() {
-            private Set<TableModelListener> listeners = new HashSet<>();
-
-            @Override
-            public int getRowCount() {
-                return n - 1;
-            }
-
-            @Override
-            public int getColumnCount() {
-                return n - 1;
-            }
-
-            @Override
-            public String getColumnName(int columnIndex) {
-                return "x" + columnIndex;
-            }
-
-            @Override
-            public Class<?> getColumnClass(int columnIndex) {
-                return int.class;
-            }
-
-            @Override
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return false;
-            }
-
-            @Override
-            public Object getValueAt(int rowIndex, int columnIndex) {
-
-                return G1[rowIndex][columnIndex];
-            }
-
-            @Override
-            public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-
-            }
-
-            @Override
-            public void addTableModelListener(TableModelListener l) {
-                listeners.add(l);
-            }
-
-            @Override
-            public void removeTableModelListener(TableModelListener l) {
-                listeners.remove(l);
-            }
-        });
+        JTable table = new JTable(new myTableModel());
 
         JScrollPane scrollPane = new JScrollPane(table);
 
         JButton button = new JButton("Accept deletion");
         JSpinner spinner = new JSpinner(new SpinnerNumberModel(0, 0, n - 1, 1));
+        JLabel label=new JLabel();
 
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                button.setVisible(false);
-                int G2[][] = new int[n - 1][n - 1];
+        button.addActionListener(e -> {
+            button.setVisible(false);
+            int G2[][] = new int[n - 1][n - 1];
 
-                for (int i = 0; i < n - 1; i++) {
-                    if (i == del) continue;
-                    for (int j = 0; j < n - 1; j++) {
-                        if (j == del) continue;
-                        if (i < del && j < del) {
-                            G2[i][j] = G1[i][j];
-                        } else if (i < del && j > del) {
-                            G2[i][j] = G1[i][j + 1];
-                        } else if (i > del && j < del) {
-                            G2[i][j] = G1[i + 1][j];
-                        } else {
-                            G2[i][j] = G1[i + 1][j + 1];
-                        }
+            for (int i = 0; i < n; i++) {
+                if (i == del) continue;
+                for (int j = 0; j < n; j++) {
+                    if (j == del) continue;
+                    if (i < del && j < del) {
+                        G2[i][j] = G1[i][j];
+                    } else if (i < del && j > del) {
+                        G2[i][j-1] = G1[i][j];
+                    } else if (i > del && j < del) {
+                        G2[i-1][j] = G1[i][j];
+                    } else if (i > del && j > del){
+                        G2[i-1][j-1] = G1[i][j];
                     }
                 }
-                G1 = G2;
             }
+            G1 = G2;
+            table.setModel(new myTableModel());
+
+            int max=0;
+            int index=0;
+
+            for (int i=0;i<n-1;i++)
+            {
+                int curr=0;
+                for (int j=0;j<n-1;j++)
+                {
+                    curr=curr+G1[i][j];
+                }
+                if (curr>max) {max=curr;index=i;}
+            }
+
+            label.setText("Максимальная полустепень исхода при x"+index+'='+max);
         });
 
         spinner.addChangeListener(e -> del = (int) spinner.getValue());
@@ -120,6 +85,7 @@ public class Main implements Runnable {
         JPanel panel = new JPanel();
         panel.add(button);
         panel.add(spinner);
+        panel.add(label);
 
         jf.setLayout(new BorderLayout());
         jf.add(panel, BorderLayout.SOUTH);
@@ -127,4 +93,54 @@ public class Main implements Runnable {
         jf.pack();
 
     }
-}
+
+    private class myTableModel implements TableModel {
+            private Set<TableModelListener> listeners = new HashSet<>();
+
+            @Override
+            public int getRowCount() {
+            return G1.length;
+        }
+
+            @Override
+            public int getColumnCount() {
+            return G1.length;
+        }
+
+            @Override
+            public String getColumnName(int columnIndex) {
+            return "x" + columnIndex;
+        }
+
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+            return int.class;
+        }
+
+            @Override
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+            return false;
+        }
+
+            @Override
+            public Object getValueAt(int rowIndex, int columnIndex) {
+
+            return G1[rowIndex][columnIndex];
+        }
+
+            @Override
+            public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+
+        }
+
+            @Override
+            public void addTableModelListener(TableModelListener l) {
+            listeners.add(l);
+        }
+
+            @Override
+            public void removeTableModelListener(TableModelListener l) {
+            listeners.remove(l);
+        }
+        }
+    }
