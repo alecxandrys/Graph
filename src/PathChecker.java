ce3d0589af356af4e7a503cedfc2fc799ae4ee6d
alecxandrys <alecxandrys@gmail.com>
2016-11-07 15:33:07 +0300

    Transfer to Callable,FutureTask and ExecutorService complete

--- src/PathChecker.java
+++ src/PathChecker.java
@@ -1,107 +1,26 @@
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.concurrent.Callable;
 
-class PathChecker extends Thread{
+class PathChecker implements Callable<ArrayList> {
 
     private int target;
 
     private boolean cameTo;
 
-    private static ArrayList<Integer> answerListOut=new ArrayList<>();
-    private static ArrayList<Integer> answerListIn=new ArrayList<>();
+    private ArrayList<Integer> circuit = new ArrayList<>();
 
-    private static ArrayList<Integer> notOutCircuit;
-    private static ArrayList<Integer> notInCircuit;
-
-    PathChecker(int target,boolean cameTo)
-    {
-        this.target=target;
-        this.cameTo=cameTo;
+    PathChecker(int target, boolean cameTo) {
+        this.target = target;
+        this.cameTo = cameTo;
     }
 
-    @Override
-    public void run() {
-        //вариант 4
-        ArrayList<Integer> frontline = new ArrayList<>();
-        Hashtable<Integer,Integer> came_from = new Hashtable<>();
-        Hashtable<Integer,Integer> cost_so_far = new Hashtable<>();
-
-        frontline.add(target);
-
-        cost_so_far.put(target,0);
-
-        came_from.put(target,-1);
-
-        int current;
-
-        while(!frontline.isEmpty())
-        {
-            current=frontline.get(0);
-            frontline.remove(0);
-            ArrayList<Integer> neighbor= neighbor(current);
-            for (Integer next:neighbor)
-            {
-                int newCost=cost_so_far.get(current)+Cost(current,next);
-                if (!cost_so_far.containsKey(next)||newCost<cost_so_far.get(next))
-                {
-                    cost_so_far.put(next,newCost);
-                    frontline.add(next);
-                    came_from.put(next,current);
-                }
-
-            }
-        }
-
-        if (cameTo) {
-            notOutCircuit=new ArrayList<>();
-            for (int i = 0; i < Task4.G1.length; i++) {
-                if (came_from.containsKey(i)) {
-                    answerListOut.add(i);
-                } else {
-                    notOutCircuit.add(i);
-                }
-            }
-        }
-        else
-        {
-            notInCircuit=new ArrayList<>();
-            for (int i = 0; i < Task4.G1.length; i++) {
-                if (came_from.containsKey(i)) {
-                    answerListIn.add(i);
-                } else {
-                    notInCircuit.add(i);
-                }
-            }
-        }
-
-        CallMain();
-    }
-
-    private void CallMain()
-    {
-        if (notOutCircuit!=null && notInCircuit!=null)
-        {
-            boolean divided=false;
-            for (Integer aNotOutCircuit : notOutCircuit) {
-                if (!notInCircuit.contains(aNotOutCircuit)) {
-                    divided = true;
-                    break;
-                }
-            }
-            Task4.SetTableModel(answerListIn,answerListOut,divided);
-        }
-
-    }
-
-
-
-    private int Cost(int current,int next) {
+    private int Cost() {
         return 1;
     }
 
-    private ArrayList<Integer> neighbor(int x)
-    {
-        ArrayList<Integer> neighbor=new ArrayList<>();
+    private ArrayList<Integer> neighbor(int x) {
+        ArrayList<Integer> neighbor = new ArrayList<>();
 
         if (cameTo) {
             for (int i = 0; i < Task4.G1[x].length; i++) {
@@ -109,9 +28,7 @@ class PathChecker extends Thread{
                     neighbor.add(i);
                 }
             }
-        }
-        else
-        {
+        } else {
             for (int i = 0; i < Task4.G1.length; i++) {
                 if (Task4.G1[i][x] == 1) {
                     neighbor.add(i);
@@ -121,5 +38,44 @@ class PathChecker extends Thread{
 
         neighbor.trimToSize();
         return neighbor;
+    }
+
+    @Override
+    public ArrayList call() throws Exception {
+        //вариант 4
+        ArrayList<Integer> frontline = new ArrayList<>();
+        Hashtable<Integer, Integer> came_from = new Hashtable<>();
+        Hashtable<Integer, Integer> cost_so_far = new Hashtable<>();
+
+        frontline.add(target);
+
+        cost_so_far.put(target, 0);
+
+        came_from.put(target, -1);
+
+        int current;
+
+        while (!frontline.isEmpty()) {
+            current = frontline.get(0);
+            frontline.remove(0);
+            ArrayList<Integer> neighbor = neighbor(current);
+            for (Integer next : neighbor) {
+                int newCost = cost_so_far.get(current) + Cost();
+                if (!cost_so_far.containsKey(next) || newCost < cost_so_far.get(next)) {
+                    cost_so_far.put(next, newCost);
+                    frontline.add(next);
+                    came_from.put(next, current);
+                }
+
+            }
+        }
+
+        for (int i = 0; i < Task4.G1.length; i++) {
+            if (came_from.containsKey(i)) {
+                circuit.add(i);
+            }
+        }
+
+        return circuit;
     }
 }
--- src/Task4.java
+++ src/Task4.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
 
 class Task4 {
 
@@ -8,10 +12,8 @@ class Task4 {
     static int G1[][];
 
     private static JFrame jf4;
-    private static long start;
 
-    Task4()
-    {
+    Task4() {
         jf4 = new JFrame("Связанность и замыкания. Чудинов Александр Алексеевич");
 
         jf4.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -32,59 +34,71 @@ class Task4 {
 
         JScrollPane scrollPane = new JScrollPane(table);
 
-        scrollPane.setPreferredSize(new Dimension(table.getWidth()+5,table.getRowHeight()*table.getRowCount()+25));
+        scrollPane.setPreferredSize(new Dimension(table.getWidth() + 5, table.getRowHeight() * table.getRowCount() + 25));
 
-        jf4.add(scrollPane, BorderLayout.NORTH);
+        JLabel circuitInList = new JLabel("Элементы обратного замыкания: ");
+        JLabel circuitOutList = new JLabel("Элементы прямого замыкания: ");
+        JLabel divideMess = new JLabel("Связанность- ");
 
-        start = System.currentTimeMillis();
-        Thread cameTo=new PathChecker(4,true);
-        cameTo.start();
-        Thread cameFrom=new PathChecker(4,false);
-        cameFrom.start();
-
-    }
-
-    static void SetTableModel(ArrayList<Integer> circuitIn,ArrayList<Integer> circuitOut, Boolean divided)
-    {
-        long finish = System.currentTimeMillis();
-
-        JLabel circuitInList=new JLabel("Элементы прямого замыкания: ");
-
-        for (Integer element:circuitIn)
-        {
-            circuitInList.setText(circuitInList.getText()+" "+element);
-        }
-
-        JLabel circuitOutList=new JLabel("Элементы обратного замыкания: ");
-
-        for (Integer element:circuitOut)
-        {
-            circuitOutList.setText(circuitOutList.getText()+" "+element);
-        }
-
-        JLabel divideMess=new JLabel("Связанность-"+!divided);
-
-        JPanel answer=new JPanel();
+        JPanel answer = new JPanel();
 
         answer.add(circuitInList);
         answer.add(circuitOutList);
         answer.add(divideMess);
 
-        JButton changeButton=new JButton("Исполнить многопоточное сравнение");
+        boolean divide = true;
+
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        FutureTask<ArrayList> circuitIn = new FutureTask<>(new PathChecker(4, true));
+        FutureTask<ArrayList> circuitOut = new FutureTask<>(new PathChecker(4, false));
+
+        long start = System.currentTimeMillis();
+
+        executor.submit(circuitIn);
+        executor.submit(circuitOut);
+
+        while (!circuitIn.isDone() && !circuitOut.isDone()) {
+
+        }
+
+        long finish = System.currentTimeMillis();
+
+        ArrayList<Integer> circuitInArray;
+        ArrayList<Integer> circuitOutArray;
+        try {
+            circuitInArray = circuitIn.get();
+            circuitOutArray = circuitOut.get();
+            for (Integer element : circuitInArray) {
+                circuitInList.setText(circuitInList.getText() + " " + (element + 1));
+            }
+            for (Integer element : circuitOutArray) {
+                circuitOutList.setText(circuitOutList.getText() + " " + (element + 1));
+            }
+            if (G1.length != circuitInArray.size() || G1.length != circuitOutArray.size()) divide = false;
+
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        divideMess.setText(divideMess.getText() + divide);
+
+        JButton changeButton = new JButton("Исполнить многопоточное сравнение");
         changeButton.addActionListener(e -> {
             jf4.setVisible(false);
             new Task5();
         });
 
-        JLabel timeMess=new JLabel("Время выполнения штатного задания (2 потока)="+(finish - start)+"мс");
+        JLabel timeMess = new JLabel("Время выполнения штатного задания (2 потока)=" + (finish - start) + "мс");
 
-        JPanel charge=new JPanel();
+        JPanel charge = new JPanel();
 
         charge.add(changeButton);
         charge.add(timeMess);
 
-        jf4.add(answer,BorderLayout.CENTER);
-        jf4.add(charge,BorderLayout.SOUTH);
+        jf4.add(scrollPane, BorderLayout.NORTH);
+        jf4.add(answer, BorderLayout.CENTER);
+        jf4.add(charge, BorderLayout.SOUTH);
         jf4.pack();
     }
 }
